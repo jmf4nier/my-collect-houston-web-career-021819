3c70ccc6aed9ddfddb7674b94002817b5ffaa25a
Jason Fournier <jmf4nier@hotmail.com>
2019-02-05 21:33:22 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,18 +2,17 @@ array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
 def my_collect(array)
   i = 0
-  new_array = Array.new
+  
   while i < array.length 
-    new_array = []
     new_array << yield(array[i])
-    
+    new_array = []
     i = i + 1
     new_array
   end
 end
 
 my_collect(array) do |name|
-   puts name.split(" ").first
+   name.split(" ").first
 end
     
 
